fe392bb62082ba4edda87d88e6c323fed67000ac
Aryan Sharma <72251324+Sharmaryan@users.noreply.github.com>
2022-03-18 19:17:59 +0530

    feat : product rendering

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Product } from "../Product/Product";
 export const Products = () => {
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const response = await axios.get("api/products");
+        setData(response.data.products);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getProducts();
+  }, []);
+
+  const [data, setData] = useState([]);
   return (
-    <div className='products'><h1>this is product listing component</h1></div>
-  )
-}
+    <div className="products">
+      <div>{data && data.map((product) => <Product product={product} />)}</div>
+    </div>
+  );
+};
