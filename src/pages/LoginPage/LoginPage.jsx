0c2ddd7a4ead9e18bad6e4e1637ca5f212eaf3e3
Aryan Sharma <72251324+Sharmaryan@users.noreply.github.com>
2022-03-26 12:54:07 +0530

    fix : path

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
-import { Login } from '../../components/Login/Login'
-import './LoginPage.css'
+import React from "react";
+import { Login } from "../../components/Login/Login";
+import "./LoginPage.css";
 export const LoginPage = () => {
   return (
-    <div className='login-page'><Login/></div>
-  )
-}
+    <div className="login-page">
+      <Login />
+    </div>
+  );
+};
