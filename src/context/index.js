a4bf0651f0a0fffaf09aaccf32eb5a9c2018b490
Aryan Sharma <72251324+Sharmaryan@users.noreply.github.com>
2022-05-25 17:11:36 +0530

    refactor : created single file for context export

---
+++ src/context/index.js
@@ -0,0 +1,4 @@
+export {useAuth, AuthProvider} from './auth-context';
+export {useProduct, ProductProvider} from './product-context';
+export {useWishlist, WishlistProvider} from './wishlist-context';
+export {useCart, CartProvider} from './cart-context';
