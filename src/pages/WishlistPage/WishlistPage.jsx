d1b647ef3f1b1bdffa3bdc62eff224c95a60795c
Aryan Sharma <72251324+Sharmaryan@users.noreply.github.com>
2022-05-27 12:18:52 +0530

    feat : added shop link

--- src/pages/WishlistPage/WishlistPage.jsx
+++ src/pages/WishlistPage/WishlistPage.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
-import { WishlistProduct } from 'components/WishlistProduct/WishlistProduct'
-import './WishlistPage.css'
+import React from "react";
+import { WishlistProduct } from "components/WishlistProduct/WishlistProduct";
+import "./WishlistPage.css";
+import { useWishlist } from "context";
+import { Link } from "react-router-dom";
 export const WishlistPage = () => {
+  const { wishlistItems } = useWishlist();
   return (
-    <div className='wishlist-page'><WishlistProduct/></div>
-  )
-}
+    <div className="wishlist-page">
+      <WishlistProduct />
+
+      {wishlistItems.length === 0 && (
+        <h1 className="text-center">
+          Oh! Such Empty 
+          <Link to="/products" className="wishlist-shop">
+            Shop Now
+          </Link>{" "}
+        </h1>
+      )}
+    </div>
+  );
+};
