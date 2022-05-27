eb4d3ce44d43a4218700b5b552d3d4c9b93d34a4
Aryan Sharma <72251324+Sharmaryan@users.noreply.github.com>
2022-05-27 10:03:07 +0530

    refactor : export

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,6 @@ export { CartPage } from "../pages/CartPage/CartPage";
 export { SignUp } from "../pages/SignUpPage/SignUp";
 export { LoginPage } from "../pages/LoginPage/LoginPage";
 export { WishlistPage } from "../pages/WishlistPage/WishlistPage";
-export { UserProfile } from "pages/UserProfile/UserProfile";
+export { UserProfile } from "../pages/UserProfile/UserProfile";
 export { PageNotFound } from "../pages/PageNotFound/PageNotFound";
 export {SingleProductPage} from '../pages/SingleProductPage/SingleProductPage';
