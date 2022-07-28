ea1c328cb91feff620b1d4dde792b00e2427de6d
Aryan Sharma <72251324+Sharmaryan@users.noreply.github.com>
2022-07-28 15:28:29 +0530

    feat : added custom  hook

--- src/pages/PageNotFound/PageNotFound.js
+++ src/pages/PageNotFound/PageNotFound.js
@@ -1,6 +1,8 @@
+import { useTitle } from 'hooks/useTitle';
 import React from 'react'
 import './PageNotFound.css';
 export const PageNotFound = () => {
+  useTitle("404 | Gotham Store");
   return (
     <h1 className='error-page'>Error 404 : Requested Page not found</h1>
   )
