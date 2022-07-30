7c92fcd6cd0814cfe2d681e8b3ca6342501b1e1a
Aryan Sharma <72251324+Sharmaryan@users.noreply.github.com>
2022-07-30 10:05:55 +0530

    styles : cart page

--- src/pages/CartPage/CartPage.jsx
+++ src/pages/CartPage/CartPage.jsx
@@ -1,28 +1,46 @@
 import React from "react";
-import { CartProduct, CartSummary} from "components";
-import { useCart } from "context/cart-context";
+import { CartProduct, CartSummary } from "components";
 import { Link } from "react-router-dom";
 import "./CartPage.css";
 import { useTitle } from "hooks/useTitle";
+import { useAddress, useCart } from "context";
 
 export const CartPage = () => {
   const { itemsAdded } = useCart();
-useTitle("Cart | Gotham Store");
+  const { selectedAddress, address } = useAddress();
+  useTitle("Cart | Gotham Store");
   return (
-    <div className="cart-page">
-      <div>
-        <CartProduct />
+    <div>
+      <div className="address">
+        <div className="address-name">
+        Address:{address
+            .filter((item) => item.addressId === selectedAddress)
+            .map((item) => (
+              <div key={item.addressId}>
+                {item.city}, {item.street}, {item.state},{item.pincode}
+              </div>
+            ))}
+        </div>
+        <Link to="/address" className="address-new">
+          add address
+        </Link>
       </div>
-      {itemsAdded.length === 0 ? (
-        <h1 className="text-center">
-          Oh! Such Empty 
-          <Link to="/products" className="wishlist-shop">
-            Shop Now
-          </Link>
-        </h1>
-      ) : (
-        <CartSummary />
-      )}
+
+      <div className="cart-page">
+        <div>
+          <CartProduct />
+        </div>
+        {itemsAdded.length === 0 ? (
+          <h1 className="text-center">
+            Oh! Such Empty
+            <Link to="/products" className="wishlist-shop">
+              Shop Now
+            </Link>
+          </h1>
+        ) : (
+          <CartSummary />
+        )}
+      </div>
     </div>
   );
 };
