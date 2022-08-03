f63bac4d14b389ab2d4a418c086a2928ba7a3842
Aryan Sharma <72251324+Sharmaryan@users.noreply.github.com>
2022-08-03 10:47:41 +0530

    styles : loader

--- src/pages/SingleProductPage/SingleProductPage.js
+++ src/pages/SingleProductPage/SingleProductPage.js
@@ -11,6 +11,7 @@ import { useTitle } from "hooks/useTitle";
 
 export const SingleProductPage = () => {
   const [singleProduct, setSingleProduct] = useState([]);
+  const [loading, setLoading] = useState(true);
   const { productId } = useParams();
   const { itemsAdded, setItemsAdded } = useCart();
   const { auth } = useAuth();
@@ -23,78 +24,83 @@ export const SingleProductPage = () => {
     (async () => {
       const response = await axios.get(`/api/products/${productId}`);
       setSingleProduct(response.data.product);
+      setLoading(false);
     })();
   }, [productId]);
 
   const { name, image, price, category } = singleProduct;
 
   return (
-    <div className="single-product">
-      <img src={image} alt="" className="single-product-image " />
+    <>
+    {loading ? <h1 className="loading">Loading...</h1> :
+      <div className="single-product">
+        <img src={image} alt="" className="single-product-image " />
 
-      <div className="card card-vertical single-product-card">
-        <p className="card-title single-product-title text-xl">{name}</p>
-        <p className="single-product-category text-m">
-          <span className="single-category">Category:</span>
-          <span className="single-category-name">{category}</span>
-        </p>
-        <div className="card-price single-category-price">₹{price}</div>
-        <p className="card-desc single-product-desc text-sm">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea impedit
-          quod adipisci facilis? Obcaecati autem odit debitis tempora fugiat
-          tenetur? Dolorum numquam voluptate sunt incidunt et odit nulla
-          consequatur laboriosam.
-        </p>
-        <div className="card-btns">
-          {productExists(itemsAdded, singleProduct) ? (
-            <Link to="/cart">
-              <button className="card-btn card-vertical-btn category-btns">
-                move to cart
+        <div className="card card-vertical single-product-card">
+          <p className="card-title single-product-title text-xl">{name}</p>
+          <p className="single-product-category text-m">
+            <span className="single-category">Category:</span>
+            <span className="single-category-name">{category}</span>
+          </p>
+          <div className="card-price single-category-price">₹{price}</div>
+          <p className="card-desc single-product-desc text-sm">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea impedit
+            quod adipisci facilis? Obcaecati autem odit debitis tempora fugiat
+            tenetur? Dolorum numquam voluptate sunt incidunt et odit nulla
+            consequatur laboriosam.
+          </p>
+          <div className="card-btns">
+            {productExists(itemsAdded, singleProduct) ? (
+              <Link to="/cart">
+                <button className="card-btn card-vertical-btn category-btns">
+                  move to cart
+                </button>
+              </Link>
+            ) : (
+              <button
+                className="card-btn card-vertical-btn category-btns"
+                onClick={() =>
+                  addToCart(
+                    singleProduct,
+                    auth,
+                    navigate,
+                    showToast,
+                    setItemsAdded,
+                    axios
+                  )
+                }
+              >
+                add to cart
               </button>
-            </Link>
-          ) : (
-            <button
-              className="card-btn card-vertical-btn category-btns"
-              onClick={() =>
-                addToCart(
-                  singleProduct,
-                  auth,
-                  navigate,
-                  showToast,
-                  setItemsAdded,
-                  axios
-                )
-              }
-            >
-              add to cart
-            </button>
-          )}
+            )}
 
-          {productExists(wishlistItems, singleProduct) ? (
-            <Link to="/wishlist">
-              <button className="card-btn category-btns">
-                move to wishlist
+            {productExists(wishlistItems, singleProduct) ? (
+              <Link to="/wishlist">
+                <button className="card-btn category-btns">
+                  move to wishlist
+                </button>
+              </Link>
+            ) : (
+              <button
+                className="card-btn category-btns"
+                onClick={() =>
+                  addToWishlist(
+                    singleProduct,
+                    auth,
+                    navigate,
+                    axios,
+                    setWishlistItems,
+                    showToast
+                  )
+                }
+              >
+                add to wishlist
               </button>
-            </Link>
-          ) : (
-            <button
-              className="card-btn category-btns"
-              onClick={() =>
-                addToWishlist(
-                  singleProduct,
-                  auth,
-                  navigate,
-                  axios,
-                  setWishlistItems,
-                  showToast
-                )
-              }
-            >
-              add to wishlist
-            </button>
-          )}
+            )}
+          </div>
         </div>
       </div>
-    </div>
+    }
+    </>
   );
 };
